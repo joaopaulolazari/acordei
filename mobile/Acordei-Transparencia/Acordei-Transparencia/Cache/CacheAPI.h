a448a8ab0c91ffebd14eb2a7693d698c33bbebf3
Deivison Servat Sporteman <deivison.sporteman@gmail.com>
2015-04-11 14:43:17 -0300

    Creating cache api

--- mobile/Acordei-Transparencia/Acordei-Transparencia/Cache/CacheAPI.h
+++ mobile/Acordei-Transparencia/Acordei-Transparencia/Cache/CacheAPI.h
@@ -1,4 +1,10 @@
-
+#import "FMDB.h"
 @interface CacheAPI : NSObject
+
+    @property (strong, nonatomic) FMDatabase *db;
+
+    + (id)sharedDatabase;
+    - (void)initDB;
+    - (FMDatabase *)getDB;
 
 @end
--- mobile/Acordei-Transparencia/Acordei-Transparencia/Cache/CacheAPI.m
+++ mobile/Acordei-Transparencia/Acordei-Transparencia/Cache/CacheAPI.m
@@ -1,6 +1,45 @@
 #import <Foundation/Foundation.h>
 #import "CacheAPI.h"
 
+
 @implementation CacheAPI : NSObject
+
+static CacheAPI *sharedDatabase = nil;
+
++ (CacheAPI *)sharedDatabase {
+    if (sharedDatabase == nil) {
+        sharedDatabase = [[super allocWithZone:NULL] init];
+    }
+    
+    return sharedDatabase;
+}
+
+- (void)initDB {
+    NSString *databaseName = @"Transparencia.db";
+    NSArray *documentPaths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+    NSString *documentDir = [documentPaths objectAtIndex:0];
+    NSString *databasePath = [documentDir stringByAppendingPathComponent:databaseName];
+    
+    _db = [FMDatabase databaseWithPath:databasePath];
+    
+    if (![_db open])
+        NSLog(@"Database problem");
+}
+
+- (FMDatabase *)getDB {
+    return _db;
+}
+
+- (id)init {
+    self = [super init];
+    if (self) {
+        // Work your initialising magic here as you normally would
+    }
+    return self;
+}
+
+- (id)copyWithZone:(NSZone *)zone {
+    return self;
+}
 
 @end
