fb82e1c126ec939e63f057e8d908c51f87f05110
Deivison Servat Sporteman <deivison.sporteman@gmail.com>
2015-04-11 21:49:19 -0300

    Refactor to use MongoDB

--- robos/Camara/src/main/java/com/acordei/robo/RoboRunner.java
+++ robos/Camara/src/main/java/com/acordei/robo/RoboRunner.java
@@ -1,10 +1,14 @@
 package com.acordei.robo;
 
 import com.acordei.robo.handler.CamaraNodeHandler;
+import com.mongodb.DB;
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 import com.scireum.open.xml.XMLReader;
 import org.xml.sax.SAXException;
-import redis.clients.jedis.Jedis;
-import redis.clients.jedis.JedisShardInfo;
 
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.FileInputStream;
@@ -16,47 +20,43 @@ import java.util.Set;
 
 public class RoboRunner {
 
+    public static int QTD_HANDLER_UPDATE = 0;
     static String DESPESA_HANDLER = "DESPESA";
     public static int QTD_HANDLER = 0;
-    static Jedis db;
+    static MongoDatabase db;
 
-    public static Jedis getDb(){
+    public static MongoDatabase getDb(){
         if ( db != null ) return db;
-        JedisShardInfo shardInfo = new JedisShardInfo("transparenciadb.redis.cache.windows.net", 6379);
-        shardInfo.setPassword("sFgits7PxyuzM6liZWLdlZBawT2KqqfdhapOsECYyOo="); /* Use your access key. */
-        System.out.println("Default conn timeout :: " + shardInfo.getConnectionTimeout());
-        System.out.println("Default so   timeout :: " + shardInfo.getSoTimeout());
-        shardInfo.setConnectionTimeout(30000000);
-        shardInfo.setSoTimeout(30000000);
-        System.out.println("updated Default conn timeout :: " + shardInfo.getConnectionTimeout());
-        System.out.println("updated Default so   timeout :: " + shardInfo.getSoTimeout());
-        db = new Jedis(shardInfo);
+
+        MongoClient cliente = new MongoClient("acordei.cloudapp.net",new MongoClientOptions.Builder()
+                .connectTimeout(999999999)
+                .heartbeatConnectTimeout(999999999)
+                .heartbeatSocketTimeout(999999999)
+                .serverSelectionTimeout(999999999)
+                .socketTimeout(999999999).build());
+
+        db = cliente.getDatabase("transparencia");
         return db;
-    }
-    public static Jedis forceReconnect(){
-        db = null;
-        return getDb();
     }
 
     static void parseXml() throws IOException, SAXException, ParserConfigurationException {
         XMLReader reader = new XMLReader();
         reader.addHandler(DESPESA_HANDLER, new CamaraNodeHandler());
         reader.parse(new FileInputStream("/home/azureuser/AnoAtual.xml"));
+        //reader.parse(new FileInputStream("/Users/deivison/Downloads/AnoAtual.xml"));
+        //debugKeys();
         System.out.println("DONE");
-        // debugKeys();
+
     }
 
-    static void debugKeys(){
+    /*static void debugKeys(){
         Set<String> list = getDb().keys("*");
         Iterator<String> keys = list.iterator();
         while(keys.hasNext()){
             String key = keys.next();
-            db.del(key);
-            //System.out.println("List of stored keys:: " + keys.next());
+            System.out.println("List of stored keys:: " + keys.next());
         }
-        System.out.println("DOne");
-
-    }
+    }*/
     public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
         RoboRunner.parseXml();
 
